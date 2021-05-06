a7b465f7534ca387d16705f6aa872c0c8bb35de9
Zach Kilboy <kilboyzc@gmail.com>
2021-05-05 23:17:20 -0500

    Still setting up test environment, eliminated unused portions of RAMPS base code.

--- growOpsv0_0_1.cpp
+++ growOpsv0_0_1.cpp
@@ -4,18 +4,6 @@
 #define X_ENABLE_PIN       38
 #define X_MIN_PIN           3
 #define X_MAX_PIN          -1 //PIN 2 is used
-/*
-#define Y_STEP_PIN         60
-#define Y_DIR_PIN          61
-#define Y_ENABLE_PIN       56
-#define Y_MIN_PIN          14
-#define Y_MAX_PIN          -1 //PIN 15 is used
-
-#define Z_STEP_PIN         46
-#define Z_DIR_PIN          48
-#define Z_ENABLE_PIN       62
-#define Z_MIN_PIN          18
-#define Z_MAX_PIN          -1 //PIN 19 is used
 
 //extruder 1
 #define E0_STEP_PIN        26
@@ -41,10 +29,3 @@
 
 #define PS_ON_PIN          12	//ATX , awake=LOW, SLEEP=High
 #define KILL_PIN           -1
-/*
-#define HEATER_0_PIN	10  // Extruder Heater
-#define HEATER_1_PIN	8
-
-#define TEMP_0_PIN		13   // ANALOG NUMBERING
-#define TEMP_1_PIN		14   // ANALOG NUMBERING
-*/
